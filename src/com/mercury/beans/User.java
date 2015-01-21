2c5c2e4b444432a94277d62f6501a6dce4f065c9
jefufree <yefuzheng2@gmail.com>
2015-01-21 17:38:57 -0500

    1

---
+++ src/com/mercury/beans/User.java
@@ -0,0 +1,62 @@
+package com.mercury.beans;
+
+public class User {
+	private int userid;
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String address;
+	private int phone;
+	private int enable;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getEnable() {
+		return enable;
+	}
+	public void setEnable(int enable) {
+		this.enable = enable;
+	}
+	
+	
+}
