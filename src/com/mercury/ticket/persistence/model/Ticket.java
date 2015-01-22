3adf31bf0702688501d31d81f7c555d0bd80255f
yihan-wei <wei.yihan89@gmail.com>
2015-01-22 13:54:37 -0500

    ticket annotation

--- src/com/mercury/ticket/persistence/model/Ticket.java
+++ src/com/mercury/ticket/persistence/model/Ticket.java
@@ -1,5 +1,14 @@
 package com.mercury.ticket.persistence.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name="tickets")
 public class Ticket {
 	private int tid;
 	private int train;
@@ -10,54 +19,74 @@ public class Ticket {
 	private int available;
 	private int sold;
 	private String deptime;
+	
+	public Ticket(){}
+	@Id
+    @Column(nullable = false)
 	public int getTid() {
 		return tid;
 	}
 	public void setTid(int tid) {
 		this.tid = tid;
 	}
+	
+	@Column
 	public int getTrain() {
 		return train;
 	}
 	public void setTrain(int train) {
 		this.train = train;
 	}
+	
+	@Column
 	public String getDep() {
 		return dep;
 	}
 	public void setDep(String dep) {
 		this.dep = dep;
 	}
+	
+	@Column
 	public String getDes() {
 		return des;
 	}
 	public void setDes(String des) {
 		this.des = des;
 	}
+	
+	@Column
 	public int getPrice() {
 		return price;
 	}
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
+	@Column
 	public int getTotal() {
 		return total;
 	}
 	public void setTotal(int total) {
 		this.total = total;
 	}
+	
+	@Column
 	public int getAvailable() {
 		return available;
 	}
 	public void setAvailable(int available) {
 		this.available = available;
 	}
+	
+	@Column
 	public int getSold() {
 		return sold;
 	}
 	public void setSold(int sold) {
 		this.sold = sold;
 	}
+	
+	@Column
 	public String getDeptime() {
 		return deptime;
 	}
